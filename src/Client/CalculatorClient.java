1cb804d57078d8bd880eb5b2c1e2219b42787854
sam9525 <sam9985246327@gmail.com>
2024-11-27 23:02:53 +1030

    Create calculator client

---
+++ src/Client/CalculatorClient.java
@@ -0,0 +1,79 @@
+package Client;
+
+import Interface.Calculator;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Scanner;
+
+public class CalculatorClient {
+
+  public CalculatorClient() {}
+
+  public static void main(String[] args) {
+    String host = (args.length < 1) ? null : args[0];
+
+    char exit;
+
+    try {
+      // Connect to the registry
+      Registry registry = LocateRegistry.getRegistry(host);
+      // Lookup the calculator
+      Calculator calculator = (Calculator) registry.lookup("Calculator");
+
+      Scanner userInput = new Scanner(System.in);
+
+      // Loop to continue the program
+      do {
+        System.out.println("Enter a number or operation:");
+
+        while (true) {
+          String x = userInput.nextLine();
+          // Checks if the input is a number
+          if (isNumber(x)) {
+            try {
+              calculator.pushValue(Integer.parseInt(x));
+            } catch (Exception e) {
+              System.err.println("Failed to push value : " + e.getMessage());
+              e.printStackTrace();
+            }
+          } else {
+            try {
+              calculator.pushOperation(x);
+              int result = calculator.delayPop(1000);
+
+              System.out.println("Result: " + result);
+              break;
+            } catch (Exception e) {
+              System.err.println(
+                "Failed to push operation : " + e.getMessage()
+              );
+              e.printStackTrace();
+            }
+          }
+        }
+
+        System.out.println("Do you want to continue? (y/n)");
+        exit = userInput.nextLine().charAt(0);
+      } while (exit == 'y');
+      userInput.close();
+    } catch (Exception e) {
+      System.err.println("Client exception: " + e.toString());
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Checks if the input is a number.
+   *
+   * @param x The input to check.
+   * @return True if the input is a number, false otherwise.
+   */
+  public static boolean isNumber(String x) {
+    try {
+      Integer.parseInt(x);
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+}
